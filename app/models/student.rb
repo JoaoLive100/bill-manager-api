2eb01e60690ae68c592bbc607ecd129c8dc156c3
João Rodrigues Parra <j-r-p@outlook.com>
2024-08-07 16:07:49 -0300

    feat: finished models

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,10 +3,10 @@ class Student < ApplicationRecord
 
     PAYMENT_METHOD = %w[
         credit_card
-        payment_slip
+        boleto
     ]
     validates :name, presence: true
-    validates :cpf, presence: true, uniqueness:true
-    validates :birthdate, presence: true
-    validates :payment_method, presence: true, inclusion: { in: payment_method.keys }
+    validates :cpf, presence: true, uniqueness:true, format: { with: /\d{3}\.\d{3}\.\d{3}-\d{2}/ }
+    validates :birthdate, allow_nil: true, format: { with: /\d{4}-\d{2}-\d{2}/ }
+    validates :payment_method, presence: true, inclusion: { in: PAYMENT_METHOD }
 end
